18b402b4108117fae0d0e50f5539b0107fd3fd73
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-12-04 01:03:42 -0600

    add package --save-dev @types/node �‍♂️�‍♀️�‍♂️�‍♂️

--- src/helpers/radioList.ts
+++ src/helpers/radioList.ts
@@ -1,63 +1,25 @@
-/*
-It is not possible to do a reverse DNS from a browser yet.
-The first part (a normal dns resolve) could be done from a browser by doing DOH (DNS over HTTPs)
-to one of the providers out there. (google, quad9,...)
-So we have to fallback to ask a single server for a list.
-*/
+//import { RadioBrowser } from 'radio-browser';
+const RadioBrowser = require('radio-browser');
 
-/**
- * Ask a specified server for a list of all other server.
- */
-function get_radiobrowser_base_urls() {
-    return new Promise((resolve, reject) => {
-        var request = new XMLHttpRequest()
-        // If you need https, please use the fixed server fr1.api.radio-browser.info for this request only
-        request.open('GET', 'http://all.api.radio-browser.info/json/servers', true);
-        request.onload = function () {
-            if (request.status >= 200 && request.status < 300) {
-                var items = JSON.parse(request.responseText).map(x => "https://" + x.name);
-                resolve(items);
-            } else {
-                reject(request.statusText);
-            }
-        }
-        request.send();
-    });
+type useFilter = {
+    limit: number,
+    by: string,
+    searchTerm: string,
+
 }
 
-/**
- * Ask a server for its settings.
- */
-function get_radiobrowser_server_config(baseurl) {
-    return new Promise((resolve, reject) => {
-        var request = new XMLHttpRequest()
-        request.open('GET', baseurl + '/json/config', true);
-        request.onload = function () {
-            if (request.status >= 200 && request.status < 300) {
-                var items = JSON.parse(request.responseText);
-                resolve(items);
-            } else {
-                reject(request.statusText);
-            }
-        }
-        request.send();
-    });
+let filter: useFilter = {
+    limit: 5,
+    by: 'tag',
+    searchTerm: 'jazz'
 }
 
-/**
- * Get a random available radio-browser server.
- * Returns: string - base url for radio-browser api
- */
-function get_radiobrowser_base_url_random() {
-    return get_radiobrowser_base_urls().then(hosts => {
-        var item = hosts[Math.floor(Math.random() * hosts.length)];
-        return item;
-    });
+
+export const start = async () => {
+    try {
+        await RadioBrowser.getStations(filter)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-get_radiobrowser_base_url_random().then((x) => {
-    console.log("-", x);
-    return get_radiobrowser_server_config(x);
-}).then(config => {
-    console.log("config:", config);
-});
